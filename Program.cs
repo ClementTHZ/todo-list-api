c39042783cf6105b30ecbba34e6dd55bce5127de
Clément THOREZ <cthorez@selectour.fr>
2025-05-05 11:26:17 +0200

    initialisation des routes get

--- Program.cs
+++ Program.cs
@@ -13,28 +13,20 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return "Hello Home Page";
+});
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/tasks", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return "Hello Tasks";
+});
+
+app.MapGet("/tasks/id", () =>
+{
+    return "Hello Task";
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
