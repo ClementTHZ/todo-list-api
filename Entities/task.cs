87f4c8c1015c1ea167f0268edaf1f81802cb3fc1
Clément THOREZ <cthorez@selectour.fr>
2025-05-06 09:48:53 +0200

    first API route Add & delete + refactoring entities

---
+++ Entities/task.cs
@@ -0,0 +1,7 @@
+public class Task
+{
+    public string title { get; set; }
+    public bool is_done = false;
+
+    public DateTime created_at = DateTime.Now;
+}
--- Program.cs
+++ Program.cs
@@ -1,32 +1,92 @@
+using System.Data;
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.Data.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
+
+builder.WebHost.ConfigureKestrel(options => options.ListenAnyIP(5000));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
+app.MapPost("/tasks/new", async (httpContext) =>
 {
-    return "Hello Home Page";
+    Task task = await httpContext.Request.ReadFromJsonAsync<Task>();
+
+    var dbfile = @"DataSource=C:\Users\ClémentTHOREZ\Documents\C#\Projet-C#\SQLITE-TodoList\BDD\todolist.db";
+    using (var db = new SqliteConnection(dbfile))
+    {
+        db.Open();
+        using (var commande = db.CreateCommand())
+        {
+            commande.CommandText = $"INSERT INTO task (title) VALUES ('{task.title}')";
+            commande.ExecuteNonQuery();
+        }
+    }
+
+    httpContext.Response.Headers.Append("content-type", "application/text");
+    await httpContext.Response.WriteAsync("Task has been created !");
 });
 
-app.MapGet("/tasks", () =>
+app.MapDelete("/tasks/{id}", async (httpContext) =>
 {
-    return "Hello Tasks";
+    string id = httpContext.Request.RouteValues["id"]?.ToString();
+    var dbfile = @"DataSource=C:\Users\ClémentTHOREZ\Documents\C#\Projet-C#\SQLITE-TodoList\BDD\todolist.db";
+    using (var db = new SqliteConnection(dbfile))
+    {
+        db.Open();
+        using (var commande = db.CreateCommand())
+        {
+            commande.CommandText = "DELETE FROM task WHERE id=@id";
+            commande.Parameters.AddWithValue("@id", id);
+            commande.ExecuteNonQuery();
+        }
+    }
+    httpContext.Response.Headers.Append("content-type", "application/text");
+    await httpContext.Response.WriteAsync("Task has been deleted !");
 });
 
-app.MapGet("/tasks/id", () =>
+app.MapGet("/tasks", async (httpContext) =>
 {
-    return "Hello Task";
+    var dbfile = @"DataSource=C:\Users\ClémentTHOREZ\Documents\C#\Projet-C#\SQLITE-TodoList\BDD\todolist.db";
+    using (var db = new SqliteConnection(dbfile))
+    {
+        db.Open();
+        using (var commande = db.CreateCommand())
+        {
+
+
+            //httpContext.Request.Path["id"]
+
+            //var id = httpContext.Request.Query["id"].ToString();
+            // commande.CommandText = $"SELECT * FROM task WHERE id={id}";
+            // using (var reader = commande.ExecuteReader())
+            // {
+            //     var response = new StringBuilder();
+            //     while (reader.Read())
+            //     {
+            //         response.AppendLine(reader["title"].ToString());
+            //     }
+
+            //     httpContext.Response.Headers.Append("content-type", "text");
+            //     await httpContext.Response.WriteAsync(response.ToString());
+            // }
+        }
+    }
 });
+
+//app.MapGet("/tasks/id", () => { return TaskServices.Getall(); });
 
 app.Run();
 
